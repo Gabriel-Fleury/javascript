408858c68490401b72f5c8515810af691bf1fd51
Gabriel Fleury Martins <gabrielfleury34@gmail.com>
2025-03-31 10:38:39 -0300

    operadores JavaScript

---
+++ aula07/operadores.js
@@ -0,0 +1,28 @@
+//OPRERADORES ARITMÉTICOS 
+
+
+/* 5+2= 7 SOMA
+5-2= 3 SUBTRAÇAO
+5*2= 10 MULTIPLICAÇAO
+5/2= 2.5 divisao
+5%2= 1 RESTO DA DIVISAO
+5**2= 25 Potencia */
+
+
+
+
+/* ORDEM DE PRECEDENCIA
+
+()
+**
+* / %
++ - */
+
+
+
+
+/* INCREMENTO
+
+var x = 5
+x = x+1    x++
+x = x-1    x-- */
