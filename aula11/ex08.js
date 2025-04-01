ffb7cc077fcfb33754b2ccbce4dc5b9f87f438fa
Gabriel Fleury Martins <gabrielfleury34@gmail.com>
2025-04-01 17:45:24 -0300

    if e else

---
+++ aula11/ex08.js
@@ -0,0 +1,7 @@
+var vel = 55
+console.log(`A velocidade do seu carro é ${vel}km/h`)
+if(vel > 60){ //codiçao simples
+    console.log('Voce ultrapassou a velocidade permitida. MULTADO')
+}
+
+console.log('Dirija sempre comcinto de segurança')
---
+++ aula11/ex09.js
@@ -0,0 +1,8 @@
+var pais = 'EUA'
+console.log(`Vivendo em ${pais}`)
+if (pais == 'BRASIL'){
+    console.log( 'Voce é Brasileiro')
+
+}else{
+    console.log('Voce é Estrangeiro')
+}
