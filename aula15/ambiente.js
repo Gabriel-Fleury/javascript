06f7ff07b51daa862a47d829d3b41684d49a792f
Gabriel Fleury Martins <gabrielfleury34@gmail.com>
2025-04-03 21:22:48 -0300

    estruturas de repetiçoes

---
+++ aula15/ambiente.js
@@ -0,0 +1,14 @@
+/* var c = 1
+while(c < 5){
+    console.log('Tudo bem')
+    c++ // 
+} */
+
+
+//----------------------------
+
+console.log('Vai começar....')
+for(var c= 1;c <= 5;c++){
+    console.log(c)
+}                  // FAZ A MSM COISA DO CODIGO DE CIMA
+console.log('FIM!')
