9d3ef34bb816dd5885e062a724a84c284a3c4e77
Gabriel Fleury Martins <gabrielfleury34@gmail.com>
2025-04-07 21:11:35 -0300

    objetos

---
+++ aula19/objeto.js
@@ -0,0 +1,11 @@
+let amigo = {nome: 'gabriel',
+sexo: 'M',
+peso: 70.0,
+engordar(p){
+    console.log('Engordar')
+    this.peso += p
+}
+    
+}
+amigo.engordar(2)
+console.log(`${amigo.nome} pesa ${amigo.peso}kg`)
